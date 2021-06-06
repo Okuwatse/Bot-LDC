c309476127006a7b55046e7f1249b33bbefb3d7d
Okuwatse <64219104+Okuwatse@users.noreply.github.com>
2021-06-06 23:06:28 +0200

    Add files via upload

---
+++ BOT_LDC.py
@@ -0,0 +1,46 @@
+#BOT LDC
+
+#Import
+import asyncio
+import math
+import json
+import ffmpeg
+import discord
+import youtube_dl
+from fonctions import *
+from datetime import datetime, timedelta
+from discord.utils import get
+from discord.ext import commands, tasks
+
+#COMMANDS
+
+# Inscription 
+@bot.command()
+async def inscription(ctx):
+    author = ctx.message.author
+    await ctx.message.delet()
+    
+
+- modification
+
+- desinscription
+
+- création d équipe dans la ligue
+
+- supprimer une équipe dans la ligue
+
+- signer_contrat
+
+- annuler_contrat
+
+- info joueur
+
+- info équipe 
+
+- info marché
+
+- set manager
+
+- unset manager
+
+bot.run("ODUwOTM1MzczMjQ0NTMwNzQ4.YLw9Ug.nN_ZOqTh6GBdf6frLCIcfoDWGfQ")
